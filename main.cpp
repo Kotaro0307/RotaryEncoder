71e61b69579671ef8292788b179f5c4178bbbfff
Kotaro3710 <46733064+Kotaro0307@users.noreply.github.com>
2019-01-26 09:47:53 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,14 +7,10 @@ Serial pc(USBTX, USBRX);
 //エンコーダ
 RotaryEncoder R1(p21, p22);
 RotaryEncoder R2(p23, p24);
-RotaryEncoder R3(p25, p26);
 
 int main(void)
 {
     while(1) {
-        pc.printf("%.2lf\n\r", R1.Get_Circumference(10, 2048));
-//        pc.printf(", %d", R2.Get_Count());
-//        pc.printf(", %d\n\r", R3.Get_Count());
-        
+        pc.printf("%d, %.2lf\n\r", R1.Get_Count(), R2.Get_Circumference(10, 2048));
     }
 }
